7df20a821bfa1972d4351e53d9526b06c82af41f
Nickson046 <118267383+Nickson046@users.noreply.github.com>
2026-02-26 21:55:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById("contact-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    document.getElementById("form-status").textContent = "Thank you! Your message has been sent.";
+    this.reset();
+});
