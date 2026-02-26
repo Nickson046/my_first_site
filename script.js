daff55325ab7e08c03a88640399ac7fca4985310
Nickson046 <118267383+Nickson046@users.noreply.github.com>
2026-02-26 22:07:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.getElementById("contact-form").addEventListener("submit", function(e){
     document.getElementById("form-status").textContent = "Thank you! Your message has been sent.";
     this.reset();
 });
+
+
+const form = document.getElementById("contact-form");
+window.addEventListener("scroll", () => {
+    const rect = form.getBoundingClientRect();
+    if(rect.top < window.innerHeight) {
+        form.classList.add("visible");
+    }
+});
